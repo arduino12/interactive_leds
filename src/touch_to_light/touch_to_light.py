0d7efbe1cdc8df1224aa8bb86ec348957502fe15
arduino12 <arad.rgb@gmail.com>
2018-05-26 12:54:55 +0000

    fix touch_to_light

--- src/touch_to_light/touch_to_light.py
+++ src/touch_to_light/touch_to_light.py
@@ -1,10 +1,12 @@
 import os
 import time
 import logging
+import functools
+from PIL import Image, ImageDraw
 
-
+from rgbmatrix import RGBMatrix #, graphics
 from infra.app import app
-from infra.modules.mpr121.mpr121 import Mpr121
+from infra.modules.sensors.mpr121 import electrodes
 from interactive_leds.src.touch_to_light import constants
 
 
@@ -12,9 +14,32 @@ class TouchToLight(app.App):
     _logger = logging.getLogger('touch_to_light')
 
 
-    def __init__(self):
+    def __init__(self, globals):
         app.App.__init__(self, constants)
-        self._modules.extend((mpr121,))
+        self._modules.extend((electrodes.mpr121.registers_tree, electrodes.mpr121.i2c_mux, electrodes.mpr121, electrodes))
+
+        if not hasattr(globals, 'matrix'):
+            globals.matrix = RGBMatrix(options=constants.RGB_MATRIX_OPTIONS)
+        self.matrix = globals.matrix
+
+        self.image = Image.new('RGB', constants.RGB_MATRIX_SIZE)
+        self.draw = ImageDraw.Draw(self.image)
+
+        self.electrodes = electrodes.Mpr121ElectrodesGrid(
+            constants.MPR121_MAP, constants.ELECTRODES_SIZE, constants.RGB_MATRIX_SIZE)
+        # self.run()
+
+    def run(self):
+        SIZE = 3
+        while True:
+            self.electrodes.update()
+            touched_electrodes = self.electrodes.get_touched()
+            for i in touched_electrodes:
+                x, y = i.mid_pixel
+                self.draw.ellipse((x - SIZE, y - SIZE, x + SIZE, y + SIZE), fill=(0, 0, 0), outline=(0, 0, 255))
+            self.matrix.SetImage(self.image, 0, 0)
+            self.draw.rectangle((0, 0) + constants.RGB_MATRIX_SIZE, fill=(0, 0, 0))
+            time.sleep(0.01)
 
     def __exit__(self):
         app.App.__exit__(self)
