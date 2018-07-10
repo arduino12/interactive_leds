08382fc61a95fbe8122d0da74eff9e15b884282f
arduino12 <arad.rgb@gmail.com>
2018-07-10 17:04:38 +0300

    game_server can now run games without a client
    Signed-off-by: arduino12 <arad.rgb@gmail.com>

--- src/game_server/game_server.py
+++ src/game_server/game_server.py
@@ -1,10 +1,16 @@
+import sys
+import time
 import pygame
 import logging
+import importlib
 from PIL import Image
+from queue import Queue
+from threading import Thread
 from rgbmatrix import RGBMatrix, graphics
 
 from . import constants
 from infra.app import app
+from infra.core import utils
 from infra.modules.sensors.mpr121 import electrodes
 
 
@@ -26,22 +32,60 @@ class GameServer(app.App):
             constants.RGB_MATRIX_SIZE)
         self.electrodes.init()
 
+        self._last_pil_string = ''
+        self.constants = constants
         self.canvas = self.matrix.CreateFrameCanvas()
 
-        font = graphics.Font()
-        font.LoadFont('/home/pi/Public/rpi-rgb-led-matrix/fonts/5x7.bdf')
+        # self._logger.error(str(sys.argv))
+        if len(sys.argv) <= 5:
+            font = graphics.Font()
+            font.LoadFont('/home/pi/Public/rpi-rgb-led-matrix/fonts/5x7.bdf')
 
-        for i, t in enumerate(constants.LOCAL_IP.split('.')):
-            graphics.DrawText(
-                self.canvas, font, 1, 7 + i * 8,
-                graphics.Color(255, 255, 0), t)
-        self.canvas = self.matrix.SwapOnVSync(self.canvas)
+            for i, t in enumerate(constants.LOCAL_IP.split('.')):
+                graphics.DrawText(
+                    self.canvas, font, 1, i * (font.height + 1) + font.height,
+                    graphics.Color(255, 255, 0), t)
+            self.canvas = self.matrix.SwapOnVSync(self.canvas)
+        else:
+            game_path = sys.argv[-1]
+            game_module = importlib.import_module(game_path)
+            game_class = getattr(
+                game_module,
+                utils.module_to_class_name(game_path.rsplit('.', 1)[1]))
+            self.game = game_class(self)
+            self._modules.append(game_module)
+
+            self._runner_q = Queue()
+            self._runner = Thread(target=self.run, daemon=True)
+            self._runner.start()
+            self.set_run(True)
+
+    def run(self):
+        status = 1
+        while status:
+            time.sleep(0.001)
+            if not self._runner_q.empty():
+                status = self._runner_q.get()
+            if status == 1:
+                continue
+
+            self.electrodes.update()
+            self.game.loop(True)
+
+    def set_run(self, is_run):
+        self._runner_q.put(2 if is_run else 1)
 
     def draw_pil_string(self, s):
         pil_image = Image.frombytes('RGB', constants.RGB_MATRIX_SIZE, s)
         # self.matrix.SetImage(pil_image)
         self.canvas.SetImage(pil_image)
         self.canvas = self.matrix.SwapOnVSync(self.canvas)
+
+    def draw_surface(self, surface):
+        pil_string = pygame.image.tostring(surface, 'RGB', False)
+        if self._last_pil_string != pil_string:
+            self._last_pil_string = pil_string
+            self.draw_pil_string(pil_string)
 
     def __exit__(self):
         app.App.__exit__(self)
